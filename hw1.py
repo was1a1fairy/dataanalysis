9314fea03711e6a755d8f142acb032bb73115122
Ари <arianna2811@yandex.ru>
2026-01-15 15:05:28 +0300

    add task2

--- hw1.py
+++ hw1.py
@@ -6,3 +6,9 @@ print(np.sum(temps))
 print(np.average(temps))
 print(np.amax(temps))
 print(np.amin(temps))
+
+# task2
+
+h1 = np.array([45, 50, 47])
+h2 = np.array([48, 46, 52])
+print(h1+h2,h1*h2,h1@h2)
