e13017a443efee6c1d3ddd357094982309d83008
Ари <arianna2811@yandex.ru>
2026-01-15 15:15:19 +0300

    add task3

--- hw1.py
+++ hw1.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 #task1
+
 temps = np.array([15.2, 16.8, 14.5, 17.0, 16.1])
 print(np.sum(temps))
 print(np.average(temps))
@@ -12,3 +13,17 @@ print(np.amin(temps))
 h1 = np.array([45, 50, 47])
 h2 = np.array([48, 46, 52])
 print(h1+h2,h1*h2,h1@h2)
+
+# task3
+
+x = np.array([
+    [20.1, 20.3, 19.8],
+    [21.0, 20.7, 20.2],
+    [19.5, 19.8, 19.3],
+    [20.8, 21.1, 20.6]
+])
+
+print(np.mean(x,axis=0))
+print(np.sum(x,axis=1))
+print(np.var(x,axis=0,ddof=1))
+print(np.argmin(np.var(x,axis=0,ddof=1)))
